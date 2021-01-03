235d6ff283194b8e45789bb3398945d3013104e0
Dominic <frogamic@protonmail.com>
2021-01-03 22:10:59 +1100

    Add OpenCL and vulkan(+32bit)

--- hardware/amdgpu.nix
+++ hardware/amdgpu.nix
@@ -4,5 +4,14 @@
   hardware.opengl = {
     enable = true;
     driSupport = true;
+    driSupport32Bit = true;
+    extraPackages32 = with pkgs.driversi686Linux; [
+      amdvlk
+    ];
+    extraPackages = with pkgs; [
+      amdvlk
+      rocm-opencl-icd
+      rocm-opencl-runtime
+    ];
   };
 }
