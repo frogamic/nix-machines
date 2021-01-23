fb6101fb82414999c4da2e7a6f3151cd11891550
Dominic <frogamic@protonmail.com>
2021-01-23 23:43:45 +1100

    Rearrange stable packages

--- services/desktop-apps.nix
+++ services/desktop-apps.nix
@@ -1,21 +1,24 @@
+{ config, pkgs, ... } :  {
+  # Required for Discord
+  nixpkgs.config.allowUnfree = true;
 
-{ config, pkgs, ... } : let
-  pkgs-stable = import ./pkgs-stable.nix;
-in {
   environment = {
     variables = {
       BROWSER = "firefox-devedition";
     };
-    systemPackages = with pkgs; [
+    systemPackages =
+    (with import ./pkgs-stable.nix; [
+      mpv
+    ]) ++
+    (with pkgs; [
       firefox-devedition-bin
       gimp
       gimpPlugins.gap
       inkscape
       krita
       yed
-      pkgs-stable.mpv
       spotify
       discord
-    ];
+    ]);
   };
 }
