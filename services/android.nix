456747802150189b04e5ba542795e2118a31f405
Dominic <frogamic@protonmail.com>
2021-03-13 21:57:36 +1100

    Add generic adb screen share wrapper

--- services/android.nix
+++ services/android.nix
@@ -1,19 +1,28 @@
 { pkgs, ... } : let
+  adbscreen = pkgs.writeScriptBin "adbscreen" ''
+    #! /bin/sh
+    args=(
+      "--render-driver" "opengles2" \
+      "--bit-rate" "20M" \
+      "$''\@" \
+    )
+    scrcpy "''${args[''\@]}"
+  '';
   fate-go = pkgs.writeScriptBin "fgo" ''
     #! /bin/sh
     adb shell monkey -p com.aniplex.fategrandorder.en \
                      -c android.intent.category.LAUNCHER 1 \
-    && scrcpy -Sw \
-              --window-title 'Fate/Grand Order' \
-              --render-driver opengles2 \
-              --crop 1080:1920:0:278 \
-              --bit-rate 20M
+                     > /dev/null \
+    && adbscreen -Sw \
+      --window-title 'Fate/Grand Order' \
+      --crop 1080:1920:0:278
   '';
 in {
   programs.adb.enable = true;
 
   environment.systemPackages = with pkgs; [
     fate-go
+    adbscreen
     adbfs-rootless
     scrcpy
     android-file-transfer
