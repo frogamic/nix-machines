e49132df2af9dabd2285104c0375c7325785c022
Dominic <frogamic@protonmail.com>
2021-03-28 16:44:12 +1100

    Add steamcmd cli client

--- services/steam.nix
+++ services/steam.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... } : {
   nixpkgs.config.allowUnfree = true;
-
+  environment.systemPackages = [
+    pkgs.steamPackages.steamcmd
+  ];
   programs.steam.enable = true;
   hardware.steam-hardware.enable = true;
 }
