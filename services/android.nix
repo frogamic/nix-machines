95f4aad279d4ba82eb058630acae678f6e66c875
Dominic Shelton <frogamic@protonmail.com>
2021-05-15 01:00:42 +1000

    Add android screenshot script

--- services/android.nix
+++ services/android.nix
@@ -1,18 +1,13 @@
 { pkgs, ... } : let
-  adbscreen = pkgs.writeScriptBin "adbscreen" ''
-    #! ${pkgs.stdenv.shell}
-    args=(
-      "--render-driver" "opengl" \
-      "--bit-rate" "12M" \
-      "$''\@" \
-    )
-    exec ${pkgs.scrcpy}/bin/scrcpy "''${args[''\@]}"
-  '';
   mkAdbApp = import ../lib/mkAdbApp.nix pkgs;
 in {
   programs.adb.enable = true;
 
   environment.systemPackages = with pkgs; [
+    adbfs-rootless
+    android-file-transfer
+    android-udev-rules
+
     (mkAdbApp {
       bin = "fgo";
       name = "com.aniplex.fategrandorder.en";
@@ -25,9 +20,21 @@ in {
       crop = "1080:2204:0:136";
       title = "Touhou LostWord";
     })
-    adbscreen
-    adbfs-rootless
-    android-file-transfer
-    android-udev-rules
+
+    (pkgs.writeScriptBin "adbshot" ''
+      #! ${pkgs.stdenv.shell}
+      FILENAME="$(date --iso-8601=seconds).png"
+      adb shell screencap -p > "$FILENAME" && \
+      ${pkgs.file}/bin/file "$FILENAME"
+    '')
+    (pkgs.writeScriptBin "adbscreen" ''
+      #! ${pkgs.stdenv.shell}
+      args=(
+        "--render-driver" "opengl" \
+        "--bit-rate" "12M" \
+        "$''\@" \
+      )
+      exec ${pkgs.scrcpy}/bin/scrcpy "''${args[''\@]}"
+    '')
   ];
 }
