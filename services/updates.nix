afe1e64c4c03567558c7f7af4b8c37c6ba2b43a4
Dominic Shelton <frogamic@protonmail.com>
2021-05-15 03:25:11 +1000

    Make autoupdate compatible with flakes

--- services/updates.nix
+++ services/updates.nix
@@ -2,12 +2,16 @@
   system.autoUpgrade = {
     enable = true;
     dates = "23:00";
-    channel = "https://nixos.org/channels/nixos-unstable";
+    flake = "github:frogamic/nix-machines";
+    flags = [
+      "--impure"
+      "--no-write-lock-file"
+    ];
   };
 
   systemd.services.nixos-upgrade.script = lib.mkOverride 50 ''
     ${config.system.build.nixos-rebuild}/bin/nixos-rebuild boot ${toString config.system.autoUpgrade.flags}
-    ${config.nix.package.out}/bin/nix-env --profile /nix/var/nix/profiles/system --delete-generations +10 --quiet
+    ${config.nix.package.out}/bin/nix-env --profile /nix/var/nix/profiles/system --delete-generations +10
     ${config.nix.package.out}/bin/nix-collect-garbage --quiet
     /nix/var/nix/profiles/system/bin/switch-to-configuration boot
   '';
