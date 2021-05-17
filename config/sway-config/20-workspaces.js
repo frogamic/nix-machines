9f8f4ecb2ea1f92cc13a89035bd31af41c68ff87
Dominic Shelton <frogamic@protonmail.com>
2021-05-17 22:21:11 +1000

    Refactor sway config

--- config/sway-config/20-workspaces.js
+++ config/sway-config/20-workspaces.js
@@ -6,9 +6,11 @@ const workspaces = [
 	'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'bracketleft', 'bracketright'
 ].map((key, x) => ({key, name: (x + 1).toString()}));
 
+console.log('bindsym {');
 workspaces.forEach(({key, name}) => {
-	console.log(`bindsym $mod+${key} workspace ${name}`);
-	console.log(`bindsym $mod+Shift+${key} move container to workspace ${name}; workspace ${name}`);
+	console.log(`$mod+${key} workspace ${name}`);
+	console.log(`$mod+Shift+${key} move container to workspace ${name}; workspace ${name}`);
 });
+console.log('}');
 
 console.log('\n### End workspaces script');
