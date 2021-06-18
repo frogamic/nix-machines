fbf3e0cfcbbaeeb0f2dcf118ca35e7388c7669af
Dominic Shelton <frogamic@protonmail.com>
2021-06-19 00:11:29 +1000

    Switch to new generation on automatic updates

--- services/updates.nix
+++ services/updates.nix
@@ -13,6 +13,6 @@
 		${config.system.build.nixos-rebuild}/bin/nixos-rebuild boot ${toString config.system.autoUpgrade.flags}
 		${config.nix.package.out}/bin/nix-env --profile /nix/var/nix/profiles/system --delete-generations +10
 		${config.nix.package.out}/bin/nix-collect-garbage --quiet
-		/nix/var/nix/profiles/system/bin/switch-to-configuration boot
+		/nix/var/nix/profiles/system/bin/switch-to-configuration switch
 	'';
 }
