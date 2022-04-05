675e4f8c171ad597750273672171d9ac8c184cb0
Dominic Shelton <frogamic@protonmail.com>
2022-04-05 11:31:45 +1000

    Switch to pipewire audio

--- services/sound.nix
+++ services/sound.nix
@@ -1,6 +1,14 @@
 { config, pkgs, ... } : {
-	sound.enable = true;
-	hardware.pulseaudio.enable = true;
+	# sound.enable = true;
+	# hardware.pulseaudio.enable = true;
 	# Explicit PulseAudio support in nixpkgs applications
-	nixpkgs.config.pulseaudio = true;
+	# nixpkgs.config.pulseaudio = true;
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+	};
+	nixpkgs.config.pipewire = true;
 }
