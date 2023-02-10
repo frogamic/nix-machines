d1634e9a736f0e396e3b998359e5b83d11afded0
Dominic Shelton <frogamic@protonmail.com>
2023-02-11 00:17:07 +1100

    Replace docker-compose with podman-compose

--- services/podman.nix
+++ services/podman.nix
@@ -5,15 +5,12 @@
 		dockerSocket.enable = true;
 	};
 	environment = {
-		variables = {
-			DOCKER_HOST = "/run/podman/podman.sock";
-		};
 		systemPackages = with pkgs; [
 			buildah
-			docker-compose
+			podman-compose
 			(pkgs.writeShellScriptBin "docker" ''
 				if [ "''${1}" == "compose" ]; then
-					${docker-compose}/bin/docker-compose "''${@:2}"
+					${podman-compose}/bin/podman-compose "''${@:2}"
 				else
 					${config.virtualisation.podman.package}/bin/podman "''${@:1}"
 				fi
