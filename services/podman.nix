636baf5ac133c54a222d18a19f0bfc15efef3c96
Dominic Shelton <frogamic@protonmail.com>
2023-02-10 14:19:29 +1100

    Add docker-compose support

--- services/podman.nix
+++ services/podman.nix
@@ -1,9 +1,23 @@
-{ pkgs,  ... } : {
+{ pkgs, config, ... } : {
 	virtualisation.podman = {
 		enable = true;
 		dockerCompat = true;
+		dockerSocket.enable = true;
 	};
-	environment.systemPackages = [
-		pkgs.buildah
-	];
+	environment = {
+		variables = {
+			DOCKER_HOST = "/run/podman/podman.sock";
+		};
+		systemPackages = with pkgs; [
+			buildah
+			docker-compose
+			(pkgs.writeShellScriptBin "docker" ''
+				if [ "''${1}" == "compose" ]; then
+					${docker-compose}/bin/docker-compose "''${@:2}"
+				else
+					${config.virtualisation.podman.package}/bin/podman "''${@:1}"
+				fi
+			'')
+		];
+	};
 }
