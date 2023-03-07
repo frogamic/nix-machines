723bb18971719aa8b69809907e60c017e3b5a647
Dominic Shelton <frogamic@protonmail.com>
2023-03-07 13:53:14 +1100

    Drop bluezfull package from bluetooth

--- services/hardware/bluetooth.nix
+++ services/hardware/bluetooth.nix
@@ -1,8 +1,5 @@
 { config, pkgs, lib, ... } : {
-	hardware.bluetooth = {
-		enable = true;
-		package = pkgs.bluezFull;
-	};
+	hardware.bluetooth.enable = true;
 	hardware.pulseaudio = lib.mkIf config.hardware.pulseaudio.enable {
 		package = pkgs.pulseaudioFull;
 		extraModules = [ pkgs.pulseaudio-modules-bt ];
