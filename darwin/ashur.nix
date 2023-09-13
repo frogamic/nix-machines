538024218a27dd5bda6f44d00cef3b4309841464
Dominic Shelton <frogamic@protonmail.com>
2023-09-13 11:23:22 +1000

    Disable auto-optimise-store on Darwin
    Work around for NixOS/nix#7273

--- darwin/ashur.nix
+++ darwin/ashur.nix
@@ -61,9 +61,7 @@
       automatic = true;
     };
     extraOptions = ''
-      auto-optimise-store = true
       experimental-features = nix-command flakes
-      extra-platforms = x86_64-darwin aarch64-darwin
     '';
   };
 
