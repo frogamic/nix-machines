77298f0cee3a0123faf4b30c519e6b61327ecea4
Dominic Shelton <frogamic@protonmail.com>
2023-12-28 14:24:49 +1100

    Update nixpkgs stable 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	inputs = {
 		flake-utils.url = "github:numtide/flake-utils";
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+		nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
 		darwin = {
 			url = "github:lnl7/nix-darwin/master";
 			inputs.nixpkgs.follows = "nixpkgs";
