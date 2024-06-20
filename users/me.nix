6d85e4ccaf46885416c5a47736956f35bee4cf96
Dominic Shelton <frogamic@protonmail.com>
2024-06-20 18:22:17 +1000

    Add myself to dialout group

--- users/me.nix
+++ users/me.nix
@@ -3,7 +3,7 @@
 		name = "dominic";
 		isNormalUser = true;
 		shell = pkgs.zsh;
-		extraGroups = [ "wheel" ];
+		extraGroups = [ "wheel" "dialout" ];
 		openssh.authorizedKeys.keys = [
 			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICzGL9KhRd2lKNuTZq1cK+4bkioGBkaMetfbzf/uuqTj dominic@enki"
 			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE5rpMMLWs8oQXYtg9wXuvsb70O0vtPX+KEK1KiJAZVO dominic@ninhursag"
