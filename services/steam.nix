0040112e7bad29827f6eb73dba26992f2c6b49f5
Dominic Shelton <frogamic@protonmail.com>
2025-09-19 11:42:49 +1000

    Add some steam rules to firewall

--- services/steam.nix
+++ services/steam.nix
@@ -1,12 +1,17 @@
 { pkgs, ... } : {
 	nixpkgs.config.allowUnfree = true;
 	environment.systemPackages = [ pkgs.steamcmd ];
-	programs.steam.enable = true;
+	programs.steam = {
+		enable = true;
+		localNetworkGameTransfers.openFirewall = true;
+		remotePlay.openFirewall = true;
+	};
 	hardware.steam-hardware.enable = true;
 
 	impermanence.persistence.user.directories = [
 		".steam"
 		".local/share/vulkan"
 		".local/share/Steam"
+		".cache/mesa_shader_cache"
 	];
 }
