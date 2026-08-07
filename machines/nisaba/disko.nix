01e36296adbd27ed7243cb380835156fef8277aa
Dominic Shelton <frogamic@protonmail.com>
2026-08-08 00:34:26 +1000

    Add nisaba

---
+++ machines/nisaba/disko.nix
@@ -0,0 +1,102 @@
+let
+  lvmOnLuks = { device, name, partitions }: {
+		inherit device;
+		type = "disk";
+		content = {
+			type = "gpt";
+			partitions = partitions // {
+				"luks_${name}" = {
+					size = "100%";
+					content = {
+						type = "luks";
+						name = "luks_${name}";
+						settings = {
+							allowDiscards = true;
+							bypassWorkqueues = true;
+						};
+						content = {
+							type = "lvm_pv";
+							vg = "lvm_${name}";
+						};
+					};
+				};
+			};
+		};
+	};
+	btrfsWithSubvols = {name, subvolumes} : {
+		type = "lvm_vg";
+		lvs = {
+			"${name}" = {
+				size = "100%FREE";
+				content = {
+					inherit subvolumes;
+					type = "btrfs";
+					extraArgs = [ "-f" ];
+					mountOptions = [
+						"compress=zstd"
+						"noatime"
+					];
+				};
+			};
+		};
+	};
+in
+{
+	disko.devices = {
+		disk = {
+			nixos = lvmOnLuks {
+				device = "/dev/disk/by-id/nvme-eui.0025388691b07e28";
+				name = "nixos";
+				partitions = {
+					ESP = {
+						size = "512M";
+						type = "EF00";
+						content = {
+							type = "filesystem";
+							format = "vfat";
+							mountpoint = "/efi";
+							mountOptions = [
+								"defaults"
+								"fmask=0077"
+								"dmask=0077"
+							];
+						};
+					};
+				};
+			};
+			persist = lvmOnLuks {
+				device = "wwn-0x5002538e49637928";
+				name = "persist";
+				partitions = {};
+			};
+			steam = lvmOnLuks {
+				device = "wwn-0x5002538e49637a4f";
+				name = "evo2";
+				partitions = {};
+			};
+		};
+		lvm_vg = {
+			lvm_nixos = btrfsWithSubvols {
+				name = "nixos";
+				subvolumes = {
+					root.mountpoint = "/";
+					nix.mountpoint = "/nix";
+				};
+			};
+			lvm_persist = btrfsWithSubvols {
+				name = "persist";
+				subvolumes = {
+					persist.mountpoint = "/mnt/persist";
+					cache.mountpoint = "/mnt/cache";
+					var_log.mountpoint = "/var/log";
+				};
+			};
+			lvm_steam = btrfsWithSubvols {
+				name = "steam";
+				subvolumes = {
+					var_lib_steam.mountpoint = "/var/lib/steam";
+				};
+			};
+		};
+	};
+}
