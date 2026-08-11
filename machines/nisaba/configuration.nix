2f09fe3df1e911174eb086ad3eb1bea62fa0744b
Dominic Shelton <frogamic@protonmail.com>
2026-08-11 23:39:01 +1000

    nisaba: Add new machine

---
+++ machines/nisaba/configuration.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, lib, ... } : {
+	imports = [
+		./disko.nix
+
+		../../services/hardware/efi.nix
+		../../services/hardware/ssd.nix
+		../../services/hardware/bluetooth.nix
+		../../services/hardware/sound.nix
+		../../services/hardware/secureboot.nix
+
+		../../services/base.nix
+	];
+
+	mine = {
+		hardware = {
+			amdgpu.enable = true;
+			amdcpu.enable = true;
+		};
+	};
+
+	boot = {
+		initrd.availableKernelModules = [
+			"nvme"
+			"ehci_pci"
+			"xhci_pci"
+			"usb_storage"
+			"usbhid"
+		];
+
+		loader = {
+			timeout = 2;
+			systemd-boot = {
+				enable = true;
+				configurationLimit = 10;
+			};
+		};
+	};
+
+	impermanence = {
+		enable = false;
+		rootFileSystem = {
+			btrfsSubvolume = "impermanent_root";
+			device = "/dev/lvm_pool/nixos";
+		};
+		persistentFilesystem = {
+			btrfsSubvolume = "persist";
+			mountPoint = "/mnt/persist";
+		};
+		users = [ "me" ];
+		persistence.directories = [
+			"/var/lib/passwords/"
+		];
+	};
+
+	users = {
+		mutableUsers = false;
+		users.me.hashedPasswordFile = "/var/lib/passwords/me";
+	};
+
+	fileSystems."/mnt/data" = {
+		device = "/dev/mapper/DataVolGrp-data";
+		fsType = "ext4";
+	};
+}
---
+++ machines/nisaba/disko.nix
@@ -0,0 +1,106 @@
+let
+  lvmOnLuks = { device, name, partitions ? {} }: {
+		inherit device;
+		type = "disk";
+		content = {
+			type = "gpt";
+			partitions = partitions // {
+				"luks_${name}" = {
+					size = "100%";
+					content = {
+						type = "luks";
+						name = "luks_${name}";
+						settings = {
+							allowDiscards = true;
+							bypassWorkqueues = true;
+						};
+						content = {
+							type = "lvm_pv";
+							vg = "lvm_${name}";
+						};
+					};
+				};
+			};
+		};
+	};
+	btrfsWithSubvols = {name, subvolumes, lvs ? {}} : {
+		type = "lvm_vg";
+		lvs = {
+			"${name}" = {
+				size = "100%FREE";
+				content = {
+					inherit subvolumes;
+					type = "btrfs";
+					extraArgs = [ "-f" ];
+					mountOptions = [
+						"compress=zstd"
+						"noatime"
+					];
+				};
+			};
+		} // lvs;
+	};
+in
+{
+	disko.devices = {
+		disk = {
+			nixos = lvmOnLuks {
+				device = "/dev/disk/by-id/nvme-eui.0025388691b07e28";
+				name = "nixos";
+				partitions = {
+					ESP = {
+						size = "512M";
+						type = "EF00";
+						content = {
+							type = "filesystem";
+							format = "vfat";
+							mountpoint = "/efi";
+							mountOptions = [
+								"defaults"
+								"fmask=0077"
+								"dmask=0077"
+							];
+						};
+					};
+				};
+			};
+			persist = lvmOnLuks {
+				device = "wwn-0x5002538e49637928";
+				name = "persist";
+			};
+			steam = lvmOnLuks {
+				device = "wwn-0x5002538e49637a4f";
+				name = "steam";
+			};
+		};
+		lvm_vg = {
+			lvm_nixos = btrfsWithSubvols {
+				name = "nixos";
+				subvolumes = {
+					root.mountpoint = "/";
+					nix.mountpoint = "/nix";
+				};
+			};
+			lvm_persist = btrfsWithSubvols {
+				name = "persist";
+				subvolumes = {
+					persist.mountpoint = "/mnt/persist";
+					cache.mountpoint = "/mnt/cache";
+					var_log.mountpoint = "/var/log";
+				};
+				lvs = {
+					swap = {
+						size = "16G";
+						content.type = "swap";
+					};
+				};
+			};
+			lvm_steam = btrfsWithSubvols {
+				name = "steam";
+				subvolumes = {
+					var_lib_steam.mountpoint = "/var/lib/steam";
+				};
+			};
+		};
+	};
+}
