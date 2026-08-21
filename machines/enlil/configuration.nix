59eec582e0084660418bf0aac46381d95c90c41e
Dominic Shelton <frogamic@protonmail.com>
2026-08-21 13:51:10 +1000

    enlil: update lm_sensors config

--- machines/enlil/configuration.nix
+++ machines/enlil/configuration.nix
@@ -27,49 +27,15 @@
 			config.boot.kernelPackages.nct6687d
 		];
 	};
+
 	programs.lm_sensors = {
-		package = pkgs.mypkgs.lm_sensors;
 		enable = true;
+		package = pkgs.mypkgs.lm_sensors;
 		kernelModules = [
 			"jc42"
 			"nct6687"
 		];
-		config = ''
-chip "k10temp-pci-*"
-	label Tctl "CPU Tctl"
-	label Tccd1 "CPU CCD1"
-
-chip "amdgpu-pci-*"
-	label edge "GPU Edge"
-	label junction "GPU Hotspot"
-	label mem "GPU Memory"
-	label fan1 "GPU Fan"
-	label vddgfx "GPU Core Voltage"
-	label PPT "GPU Power (PPT)"
-	label sclk "GPU Core Clock"
-	label mclk "GPU Memory Clock"
-	label pwm1 "GPU Fan %"
-
-chip "nvme-pci-0100"
-	label temp1 "NVMe 1TB Composite"
-	label temp2 "NVMe 1TB Sensor 1"
-	label temp3 "NVMe 1TB Sensor 2"
-
-chip "nvme-pci-0400"
-	label temp1 "NVMe 2TB Composite"
-	label temp2 "NVMe 2TB Sensor 1"
-	label temp3 "NVMe 2TB Sensor 2"
-
-bus "i2c-0" "SMBus PIIX4 adapter port 0 at 0b00"
-chip "jc42-i2c-0-18"
-	label temp1 "DIMM A1"
-chip "jc42-i2c-0-19"
-	label temp1 "DIMM A2"
-chip "jc42-i2c-0-1a"
-	label temp1 "DIMM B1"
-chip "jc42-i2c-0-1b"
-	label temp1 "DIMM B2"
-		'';
+		config = builtins.readFile ./sensors3.conf;
 	};
 
 	boot = {
