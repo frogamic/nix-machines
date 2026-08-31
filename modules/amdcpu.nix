ab3e95d4a7e5de1901056425377a5350c1221cab
Dominic Shelton <frogamic@protonmail.com>
2026-08-31 11:03:41 +1000

    amdcpu: Add undervolting option

--- modules/amdcpu.nix
+++ modules/amdcpu.nix
@@ -1,14 +1,30 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
-	inherit (lib) mkIf mkEnableOption;
+	inherit (lib) getExe mkIf mkEnableOption mkOption optional optionals types;
+	inherit (builtins) toString;
+
+	doVOffset = cfg.voltageOffset != 0;
+	ryzen-smu-cli-src = pkgs.fetchFromGitHub {
+		owner = "frogamic";
+		repo = "ryzen-smu-cli";
+		rev = "0.0.3";
+		hash = "sha256-2HtN15aGfdV3B+AdCQ19Fg0WsV3zkCYxyclMkq1aKac=";
+	};
+	ryzen-smu-cli = pkgs.callPackage "${ryzen-smu-cli-src}/package.nix" { linuxPackages = config.boot.kernelPackages; };
 
 	cfg = config.mine.hardware.amdcpu;
 in
 
 {
-	options.mine.hardware.amdcpu = {
+	options.mine.hardware.amdcpu = with types; {
 		enable = mkEnableOption "AMD CPU defaults";
+		voltageOffset = mkOption {
+			default = 0;
+			example = -30;
+			description = "Set a cpu core voltage offset (e.g. undervolt) in millivolts";
+			type = int;
+		};
 	};
 
 	config = mkIf cfg.enable {
@@ -18,14 +34,16 @@ in
 		};
 
 		boot = {
+			kernelModules = [
+				"zenpower"
+			] ++ optional doVOffset "ryzen-smu";
 			blacklistedKernelModules = [ "k10temp" ];
 			extraModulePackages = with config.boot.kernelPackages; [
 				zenpower
-			];
+			] ++ optional doVOffset ryzen-smu;
 		};
 
 		programs.lm_sensors = {
-			kernelModules = [ "zenpower" ];
 			config = ''
 chip "zenpower-pci-*"
 	label temp1 "CPU die"
@@ -40,5 +58,32 @@ chip "zenpower-pci-*"
 '';
 		};
 
+		environment.systemPackages = optionals doVOffset [ ryzen-smu-cli ];
+
+		systemd.services.ryzen-smu-voltage-offset = mkIf doVOffset {
+			description = "Apply per-core CPU voltage offset";
+
+			unitConfig.ConditionPathExists = "/sys/kernel/ryzen_smu_drv/mp1_smu_cmd";
+
+			after = [
+				"sleep.target"
+				"hibernate.target"
+				"hybrid-sleep.target"
+				"suspend-then-hibernate.target"
+			];
+			wantedBy = [
+				"multi-user.target"
+				"suspend.target"
+				"hibernate.target"
+				"hybrid-sleep.target"
+				"suspend-then-hibernate.target"
+			];
+
+			serviceConfig = {
+				Type = "oneshot";
+				ExecStart = "${getExe ryzen-smu-cli} -o ${toString cfg.voltageOffset}";
+				RemainAfterExit = true;
+			};
+		};
 	};
 }
